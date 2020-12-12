819b1a823ab414756f652518b2f4dbc7ba2801c2
Dante Calderon <mail@dantecalderon.dev>
2020-12-11 23:47:36 -0500

    📙 Migrate to PSQL

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5460),
+        database: env('DATABASE_NAME', 'postgres'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
+        schema: 'public',
       },
       options: {
         useNullAsDefault: true,
